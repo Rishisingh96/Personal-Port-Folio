8bfc523b5a080fa404bd8843c1391af59edeab3f
rishisingh <rishicoding9838@gmail.com>
2024-03-15 17:29:37 +0530

    Your commit message

---
+++ src/components/About.jsx
@@ -0,0 +1,65 @@
+import { useState } from "react";
+import bannerImage from "../assets/bi1.png";
+const About = () => {
+  const [data, setData] = useState({
+    image: bannerImage,
+    title: "Java Developer & WebDeveloper",
+    desc1: `I am proficient in Java, Servlet, JSP, JavaScript, C, C++, CSS, HTML & SQL, JDBC. Starting to learn "Spring
+    Framework". I can learn any skill very quickly. Effective
+    time management skills to meet deadlines. I would
+    like to start my career as an entry level software
+    engineer with a reputed firm driven by technology.
+    `,
+    desc0: `[PERSONAL PROJECTS]`,
+    desc2: `1 - {Gym Web (Website) (2021)}-
+    By using this website, you can give all the information about yourself online
+    in Delhi Gym Club, register, login and create a profile. https:https://github.com/Rishisingh96/GymWebJava we have used frontend and backend technology and connected it to the
+    server - HTML, CSS, Java(servlet), JSP, JDBC
+   
+`,
+    desc3: ` 2 - Chat Application (Desktop App) (12/2022 - 01/2023)-
+After completing Java I created a GUI desktop app project using Swing. I am using core Java concepts like threading, exception handling and
+networking concepts. Using this project we can easily chat between
+client to server and server to client.`,
+    actionButton: {
+      title: "Read More..",
+      link: "/readmore",
+    },
+  });
+  return (
+    <>
+      <div className="main-container bg-gray-100 border py-16">
+        <h1 className="text-center pb-16  text-5xl underline font-bold">
+          About Me
+        </h1>
+
+        <div className="flex items-center">
+          {/* image container */}
+          <div className="w-full flex justify-center">
+            <img
+              className=" rounded-full my-2 shadow-lg h-45 w-40"
+              src={data.image}
+              alt="Rishi singh"
+            />
+          </div>
+
+          {/* text container  */}
+          <div className="w-full  flex justify-center">
+            <div className="space-y-5   w-2/3">
+              <h1 className="text-5xl  font-semibold ">{data.title}</h1>
+              <p>{data.desc1}</p>
+              <p>{data.desc0}</p>
+              <p>{data.desc2}</p>
+              <p>{data.desc3}</p>
+              <button className="bg-orange-500 px-3 py-2 text-2xl rounded-full shadow-lg">
+                {data.actionButton.title}
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default About;
